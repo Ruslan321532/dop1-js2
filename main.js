7207fbe764964cf097cbf407dd41dad9648c261e
ruslan <ruslanburalkiev8@gmail.com>
2022-11-09 20:45:41 +0600

    rusya

---
+++ main.js
@@ -0,0 +1,15 @@
+///dop1
+function minMax(arr){
+    return [Math.max(...arr), Math.min(...arr)];
+  }
+  console.log(minMax([1,2,3,4,5]));
+
+function minMax(arr1){
+    return [Math.max(...arr1), Math.min(...arr1)];
+  }
+  console.log(minMax([1,2,-3,4,5]));
+
+function minMax(arr2){
+    return [Math.max(...arr2), Math.min(...arr2)];
+  }
+  console.log(minMax([1,9,3,4,-5]));
